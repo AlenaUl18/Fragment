e146c42eaf65ba19bb85dec541b3fc698deeaaab
AlenaUl18 <113842226+AlenaUl18@users.noreply.github.com>
2023-03-04 00:46:13 +0300

    Передача данных между фрагментами

--- app/src/main/java/com/example/my2/BlankFragment.java
+++ app/src/main/java/com/example/my2/BlankFragment.java
@@ -12,32 +12,41 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class BlankFragment extends Fragment {
     public BlankFragment() {
         super(R.layout.fragment_blank);
     }
     private final static String TAG = "ContentFragment";
+    private Context context;
 
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
         Log.d(TAG, "onAttach");
+        Toast.makeText(context, "onAttach", Toast.LENGTH_LONG).show();
+        this.context = context;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate");
+        Toast.makeText(context, "onCreate", Toast.LENGTH_LONG).show();
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         Log.d(TAG, "onCreateView");
+        Toast.makeText(context, "onCreateView", Toast.LENGTH_LONG).show();
         return inflater.inflate(R.layout.fragment_blank, container, false);
+
     }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -51,52 +60,68 @@ public class BlankFragment extends Fragment {
         Drawable draw = getResources().getDrawable(R.drawable.movie_player_play_video_svgrepo_com);
         image.setImageDrawable(draw);
         Log.d(TAG, "onViewCreated");
+        Toast.makeText(context, "onViewCreated", Toast.LENGTH_LONG).show();
+        Button button = view.findViewById(R.id.button3);
+        button.setOnClickListener(v -> { Bundle result = new Bundle();
+            result.putString("bundleKey", String.valueOf("Аня"));
+            getParentFragmentManager().setFragmentResult("requestKey", result); getParentFragmentManager().beginTransaction().replace(R.id.fragment_container_view, BlankFragment2.class, null).commit();});
     }
     @Override
     public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
         super.onViewStateRestored(savedInstanceState);
         Log.d(TAG, "onViewStateRestored");
+        Toast.makeText(context, "onViewStateRestored", Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void onStart() {
         super.onStart();
         Log.d(TAG, "onStart");
+        Toast.makeText(context, "OnStart", Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void onResume() {
         super.onResume();
         Log.d(TAG, "onResume");
+        Toast.makeText(context, "OnResume", Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void onPause() {
         super.onPause();
         Log.d(TAG, "onPause");
+        Toast.makeText(context, "OnPause", Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void onStop() {
         super.onStop();
         Log.d(TAG, "onStop");
+        Toast.makeText(context, "OnStop", Toast.LENGTH_LONG).show();
+        if (getActivity().isFinishing()) {
+            getActivity().finish();
+        }
     }
 
     @Override
     public void onDestroyView() {
-        super.onDestroyView();
         Log.d(TAG, "onDestroyView");
+        Toast.makeText(context, "onDestroyView", Toast.LENGTH_LONG).show();
+        super.onDestroyView();
     }
 
     @Override
     public void onDestroy() {
-        super.onDestroy();
         Log.d(TAG, "onDestroy");
+        Toast.makeText(context, "onDestroy", Toast.LENGTH_LONG).show();
+        super.onDestroy();
     }
 
     @Override
     public void onDetach() {
-        super.onDetach();
         Log.d(TAG, "onDetach");
+        Toast.makeText(context, "onDetach", Toast.LENGTH_LONG).show();
+        super.onDetach();
     }
 }
--- app/src/main/java/com/example/my2/BlankFragment2.java
+++ app/src/main/java/com/example/my2/BlankFragment2.java
@@ -2,14 +2,35 @@ package com.example.my2;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class BlankFragment2 extends Fragment {
     public BlankFragment2() {
         super(R.layout.fragment_blank2);
+    }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getParentFragmentManager().setFragmentResultListener("requestKey", this, (key, bundle) -> {
+            String result = bundle.getString("bundleKey");
+            TextView text = (TextView) getView().findViewById(R.id.editTextTextPersonName4);
+            text.setText(result);
+        });
+        Button button = view.findViewById(R.id.button5);
+        button.setOnClickListener(v -> {
+            Bundle result = new Bundle();
+            result.putString("bundleKey", String.valueOf("Вы зашли в аккаунт"));
+            getParentFragmentManager().setFragmentResult("requestKey", result);
+
+            getParentFragmentManager().beginTransaction().replace(R.id.fragment_container_view, BlankFragment.class, null).commit();
+        });
     }
 }
