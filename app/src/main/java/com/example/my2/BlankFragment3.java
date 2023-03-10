4edb9e5342307e9af1223e83df25f08ba20ce2b6
Ulanova Alyona <alna.ulanova2019@mail,ru>
2023-03-10 21:02:35 +0300

    Создание трех фрагментов

---
+++ app/src/main/java/com/example/my2/BlankFragment3.java
@@ -0,0 +1,14 @@
+package com.example.my2;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+public class BlankFragment3 extends Fragment {
+    public BlankFragment3() {
+
+    }
+}
---
+++ app/src/main/java/com/example/my2/BlankFragment4.java
@@ -0,0 +1,16 @@
+package com.example.my2;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BlankFragment4 extends Fragment {
+    public BlankFragment4() {
+
+    }
+
+}
