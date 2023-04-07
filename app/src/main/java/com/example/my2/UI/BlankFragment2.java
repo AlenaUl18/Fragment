5d9a42405d29ec9cc497757501395ee408375188
Ulanova Alyona <alna.ulanova2019@mail,ru>
2023-04-07 23:02:10 +0300

    создание текстового файла в app-specific storage

--- app/src/main/java/com/example/my2/UI/BlankFragment2.java
+++ app/src/main/java/com/example/my2/UI/BlankFragment2.java
@@ -23,13 +23,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.my2.R;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class BlankFragment2 extends Fragment {
-
+    String filename = "Cinema";
     public BlankFragment2() {
         super(R.layout.fragment_blank2);
     }
@@ -37,11 +43,22 @@ public class BlankFragment2 extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         Button button = view.findViewById(R.id.button5);
-        button.setOnClickListener(new View.OnClickListener(){
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+                String name = String.valueOf(((EditText) view.findViewById(R.id.editTextTextPersonName4)).getText());
+                String prom = String.valueOf(((EditText) view.findViewById(R.id.editTextTextPersonName6)).getText());
+                String pass = String.valueOf(((EditText) view.findViewById(R.id.editTextTextPassword4)).getText());
+            File file = new File(getContext().getFilesDir(), filename);
+            try (FileOutputStream fos = getContext().openFileOutput(filename, Context.MODE_PRIVATE)) {
+                fos.write((name+"\n").getBytes());
+                fos.write((prom+"\n").getBytes());
+                fos.write((pass+"\n").getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
                 Navigation.findNavController(view).navigate(R.id.action_BlankFragment2_to_BlankFragment5);
             }
         });
-        }
+    }
 }
--- app/src/main/java/com/example/my2/UI/BlankFragment5.java
+++ app/src/main/java/com/example/my2/UI/BlankFragment5.java
@@ -2,17 +2,53 @@ package com.example.my2.UI;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.my2.R;
 
-public class BlankFragment5 extends Fragment {
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
+public class BlankFragment5 extends Fragment {
+    String filename = "Cinema";
     public BlankFragment5() {
         super (R.layout.fragment_blank5);
+    }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        FileInputStream fis;
+        String data[];
+        try {
+            fis = getContext().openFileInput(filename);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        InputStreamReader inputStreamReader = new InputStreamReader(fis, StandardCharsets.UTF_8);
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader reader = new BufferedReader(inputStreamReader);
+        try {
+            String line = reader.readLine();
+            while (line != null) {
+                stringBuilder.append(line).append('\n');
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            data = stringBuilder.toString().split("\n");
+        }
+        ((TextView)view.findViewById(R.id.name1)).setText(data[0]);
+        ((TextView)view.findViewById(R.id.textView)).setText(data[1]);
     }
 }
