30157a601a869289d6f668917f6e8851c335443a
Ulanova Alyona <alna.ulanova2019@mail,ru>
2023-04-11 00:23:51 +0300

    создание текстового файла в общем хранилище

--- app/src/main/java/com/example/my2/UI/BlankFragment2.java
+++ app/src/main/java/com/example/my2/UI/BlankFragment2.java
@@ -17,6 +17,7 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
@@ -28,15 +29,23 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.my2.R;
+import com.example.my2.ViewModels.RegistPattern;
 import com.google.android.material.textfield.TextInputLayout;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class BlankFragment2 extends Fragment {
+    RegistPattern viewPattern;
     public BlankFragment2() {
         super(R.layout.fragment_blank2);
+    }
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        viewPattern = new ViewModelProvider(this).get(RegistPattern.class);
     }
 
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -45,6 +54,14 @@ public class BlankFragment2 extends Fragment {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String Name = String.valueOf(((EditText) view.findViewById(R.id.editTextTextPersonName4)).getText());
+                String Promocode = String.valueOf(((EditText) view.findViewById(R.id.editTextTextPersonName6)).getText());
+                try {
+                    viewPattern.createAppSpec(requireContext(), "app_specific", Name + Promocode);
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+                viewPattern.CreateExternal(requireContext(), "external", Name + Promocode);
                 Navigation.findNavController(view).navigate(R.id.action_BlankFragment2_to_BlankFragment5);
             }
         });
--- app/src/main/java/com/example/my2/ViewModels/RegistPattern.java
+++ app/src/main/java/com/example/my2/ViewModels/RegistPattern.java
@@ -1,13 +1,14 @@
 package com.example.my2.ViewModels;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.os.Environment;
 
 import androidx.lifecycle.ViewModel;
 
-import com.example.my2.Manifest;
-
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -22,4 +23,18 @@ public void createAppSpec (Context context, String filename, String fileContent)
         throw new RuntimeException(e);
     }
 }
+    public void CreateExternal(Context context, String fileName, String fileInformation) {
+        if (context.getApplicationContext().checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            File filePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+            File file = new File(filePath, fileName);
+            FileOutputStream outStream;
+            try {
+                outStream = new FileOutputStream(file);
+                outStream.write(fileInformation.getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
